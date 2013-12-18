3bb389e03feb67ae6c085881466e704752aae926
Joseph N. Paulson <jpaulson@umiacs.umd.edu>
2013-12-18 16:01:22 -0500

    Update ui.r

--- ui.r
+++ ui.r
@@ -11,11 +11,11 @@ download_not_installed(required_packages)
 
 if(!(require("rCharts"))){
 	devtools::install_github('rCharts', 'ramnathv')
-    require("rCharts")
+	require("rCharts")
 }
 if(!(require("ShinyDash"))){
 	devtools::install_github("ShinyDash", "trestletech")
-    require("ShinyDash")
+	require("ShinyDash")
 }
 
 reactiveNetwork <- function (outputId) {
@@ -29,24 +29,24 @@ shinyUI(bootstrapPage(
 	gridsterItem(col = 1, row = 1, size.x = 2, size.y = 3,
             sliderInput("Year", "Choose Year",
             min = 1970, max = 2013, value = 1970, format="####")),
-    gridsterItem(col = 3, row = 1, size.x = 2, size.y = 9,
-			tags$head(tags$style(type="text/css", "select { height: 30px; width: 300px; fontSize:2}")),
-			selectInput("type", "Choose Descriptive Statistics:",
-					list("Average Degree" = "Degree Average",
-						"Clustering Coefficient" = "Clustering Coefficient",
-						"Largest Strongly Connected Component"= "LSCC",
-						"Network Density"= "Network Density (%)")),
+    	gridsterItem(col = 3, row = 1, size.x = 2, size.y = 9,
+		tags$head(tags$style(type="text/css", "select { height: 30px; width: 300px; fontSize:2}")),
+		selectInput("type", "Choose Descriptive Statistics:",
+			list("Average Degree" = "Degree Average",
+				"Clustering Coefficient" = "Clustering Coefficient",
+				"Largest Strongly Connected Component"= "LSCC",
+				"Network Density"= "Network Density (%)")),
 			plotOutput("scatter", height="230px")),
 
-    gridsterItem(col = 1, row = 4, size.x = 2, size.y = 12,
-			includeHTML("./Data/graph.js"),
-			reactiveNetwork(outputId = "mainnet")),
+    	gridsterItem(col = 1, row = 4, size.x = 2, size.y = 12,
+		includeHTML("./Data/graph.js"),
+		reactiveNetwork(outputId = "mainnet")),
 
-	gridsterItem(col = 3, row = 10, size.x = 2, size.y = 8,
-			htmlOutput("List")),
+    	gridsterItem(col = 3, row = 10, size.x = 2, size.y = 8,
+		htmlOutput("List")),
 
-		gridsterItem(col = 1, row = 16, size.x = 2, size.y = 2,tags$p("",
-			tags$a(href = "https://github.com/htalukder/musicwriters", "Source Code", target="_blank")),
-			tags$p("Copyright:",tags$a(href = "http://cbcb.umd.edu/~htalukde", "Hisham Talukder", target="_blank")))
+    	gridsterItem(col = 1, row = 16, size.x = 2, size.y = 2,tags$p("",
+		tags$a(href = "https://github.com/htalukder/musicwriters", "Source Code", target="_blank")),
+		tags$p("Copyright:",tags$a(href = "http://cbcb.umd.edu/~htalukde", "Hisham Talukder", target="_blank")))
 	)
 ))
